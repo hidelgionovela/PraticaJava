6ec8ee47d97485ca351f2d0f616b8f9df3833fff
Hidélgio Novela <hidelgionovela@gmail.com>
2022-10-25 23:06:58 +0200

    Update Main2.java

--- src/cursojava/Exceptions/Main2.java
+++ src/cursojava/Exceptions/Main2.java
@@ -9,5 +9,16 @@ package cursojava.Exceptions;
  * @author Latitude 5500
  */
 public class Main2 {
+    public static void main(String[] args) {
+        
+//        try and catch block
+
+    try{
+        int a = 5 / 0;
+        
+    }catch(Exception e){
+        System.out.println("");
+    }
+    }
     
 }
