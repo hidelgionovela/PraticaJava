688a0f7a5f37426b58cc69a8caae3ec2270befd1
Hidélgio Novela <hidelgionovela@gmail.com>
2022-10-27 15:10:33 +0200

    estudando o finally block

--- src/cursojava/Exceptions/Main2.java
+++ src/cursojava/Exceptions/Main2.java
@@ -1,4 +1,3 @@
-
 package cursojava.Exceptions;
 
 /**
@@ -6,16 +5,33 @@ package cursojava.Exceptions;
  * @author Hidelgio Novela
  */
 public class Main2 {
-    public static void main(String[] args) {
-        
-//        try and catch block
 
-    try{
-        int a = 5 / 0;
+    public static void main(String[] args) {
+
+//        try and catch block with finally block
+//        Usamos o bloco finally quando temos instrucoes que queremos executar mesmo que ocorra um erro assim como nao. 
+//         Ex1:(Tendo um erro)
+        try {
+
+            int a = 5 / 0;
+
+        } catch (ArithmeticException e) {
+            System.out.println("Tens uma excepcao");
+        } finally {
+            System.out.println("Este e o bloco final");
+        }
         
-    }catch(Exception e){
-        System.out.println("");
+//        Ex2: (Sem um erro)
+        
+          try {
+
+            int a = 4 / 2;
+
+        } catch (ArithmeticException e) {
+            System.out.println("Tens uma excepcao");
+        } finally {
+            System.out.println("Este e o bloco final");
+        }
+
     }
-    }
-    
 }
