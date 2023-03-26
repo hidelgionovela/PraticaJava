1f3213d1e335f8751be29fdf41b0b691f6375891
Hidélgio Novela <hidelgionovela@gmail.com>
2023-03-26 23:06:20 +0200

    Update CursoJava.java

--- src/cursojava/CursoJava.java
+++ src/cursojava/CursoJava.java
@@ -13,6 +13,16 @@ public class CursoJava {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Ola Mundo!");
+        
+        
+        for (int i = 0; i < 1000000000; i++) {
+            for (int j = 0; j < 100000; j++) {
+                 System.out.println(i);
+                 System.out.println(j);
+            }
+           
+            
+        }
     }
     
 }
